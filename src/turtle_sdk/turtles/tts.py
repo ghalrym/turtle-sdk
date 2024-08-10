18fe5bb1cf3de1528eda9e212143f91d31ba1eca
ghalrym <andrewreed2015@gmail.com>
2024-08-10 13:26:36 -0700

    convert tensor to wav bytes for tts

--- src/turtle_sdk/turtles/tts.py
+++ src/turtle_sdk/turtles/tts.py
@@ -1,3 +1,6 @@
+from io import BytesIO
+
+import torchaudio
 from bale_of_turtles import TurtleTool, use_state
 from mini_tortoise_tts import TextToSpeech, safe_load_voice
 
@@ -11,3 +14,8 @@ class MiniTortoiseTtsTurtle(TurtleTool):
     @use_state("mini-tortoise-tts-say", ["mini_tortoise_tts_say"])
     def say(self, mini_tortoise_tts_say: str, **kwargs):
         audio = self.tts.generate(mini_tortoise_tts_say)
+        bytesio = BytesIO()
+        torchaudio.save(
+            bytesio, audio.squeeze(0).cpu(), 24000, format="wav", bits_per_sample=32
+        )
+        self.update_state(audio_out_byte=bytesio)
