049e1405344bd0f72154a381b7c6defe5870e36d
ghalrym <andrewreed2015@gmail.com>
2024-08-10 02:48:34 -0700

    Add MiniTortoiseTts Turtle

---
+++ src/turtle_sdk/turtles/tts.py
@@ -0,0 +1,14 @@
+from bale_of_turtles import TurtleTool, use_state
+
+from mini_tortoise_tts import TextToSpeech, safe_load_voice
+
+
+class MiniTortoiseTtsTurtle(TurtleTool):
+
+    def __init__(self, voice: str):
+        super().__init__()
+        self.tts = TextToSpeech(safe_load_voice(voice))
+
+    @use_state("mini-tortoise-tts-say", ["mini_tortoise_tts_say"])
+    def say(self, mini_tortoise_tts_say: str, **kwargs):
+        audio = self.tts.generate(mini_tortoise_tts_say)
